bdb9cde113171d264ece48c2ba4271de49468f24
haha <zn8y54u@gmail.com>
2019-05-31 18:25:46 +0800

    V1.1 add score and gameover hint

--- breakout/GameScene.swift
+++ breakout/GameScene.swift
@@ -12,15 +12,24 @@ import GameplayKit
 class GameScene: SKScene,SKPhysicsContactDelegate {
     var ball:SKSpriteNode!
     var paddle:SKSpriteNode!
+    var scoreBoard:SKLabelNode!
+    var score:Int = 0
+    
+    func initGame() {
+        
+        score = 0
+    }
     
     override func didMove(to view: SKView) {
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         paddle = self.childNode(withName: "paddle") as! SKSpriteNode
+        scoreBoard = self.childNode(withName: "score") as! SKLabelNode
         //let ball move
         ball.physicsBody?.applyImpulse(CGVector(dx: 80, dy: 80 ))
         
         let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
         border.friction = 0
+        border.categoryBitMask = 4
         self.physicsBody = border
         
         self.physicsWorld.contactDelegate = self
@@ -44,12 +53,21 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         let nameA = contact.bodyA.node?.name
         let nameB = contact.bodyB.node?.name
-        if(nameA=="block"&&nameB=="ball" || nameB=="block"&&nameA=="ball"){
+        let maskA = contact.bodyA.categoryBitMask
+        let maskB = contact.bodyB.categoryBitMask
+        
+
+        if(maskA==3 || maskB==3){
             if(nameA=="block"){
                 contact.bodyA.node?.removeFromParent()
             }else if(nameB=="block"){
                 contact.bodyB.node?.removeFromParent()
             }
+            score+=1
+            scoreBoard.text = "SCORE: "+String(score)
+        }else if(maskA==10 || maskB==10){
+            ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0 )
+            self.childNode(withName: "gameoverLabel")?.isHidden = false
         }
     }
 }
